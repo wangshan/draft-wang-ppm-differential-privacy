9f88426c7f1b958cc41f028fcad5a222b7eb9f7e
Junye Chen <junyec@apple.com>
2023-10-17 15:37:37 +0800

    Clean up text for Client-DP
    For DP policy execution: added a convenience method
    run_dp_policy_with_vdaf to demonstrate how to run a DP policy with
    a VDAF.
    For Client-DP: specify randomized response sampler and specify how
    to instantiate Prio3MultiHotHistogram with the maximum number of 1s.

--- poc/mechanism/binary_randomized_response.py
+++ poc/mechanism/binary_randomized_response.py
@@ -4,6 +4,7 @@ https://arxiv.org/pdf/2211.10082.pdf.
 """
 
 import math
+import random
 
 from mechanism import DpMechanism
 
@@ -18,24 +19,48 @@ class BinaryRandomizedResponse(DpMechanism):
         self.p = 1.0 / (math.exp(eps0) + 1.0)
 
     def add_noise(self, data: DataType) -> DataType:
-        # TODO: Implement this (#10).
-        raise NotImplementedError()
+        # Apply binary randomized response at each coordinate, based on
+        # Appendix C.1.1 of {{MJTB+22}}.
+        return list(map(
+            lambda x: 1 - x if self.coin_flip() else x,
+            data
+        ))
 
     def sample_noise(self, dimension: int) -> DataType:
-        # TODO: Implement this (#10).
-        raise NotImplementedError()
+        # Sample binary randomized response at each coordinate on an all zero
+        # vector.
+        return [int(self.coin_flip()) for coord in range(dimension)]
 
     def debias(self,
                data: DataType,
                meas_count: int) -> DebiasedDataType:
-        # TODO: Implement this (#10).
-        raise NotImplementedError()
+        # Debias the data based on Appendix C.1.2 of {{MJTB+22}}.
+        exp_eps = math.exp(self.eps0)
+        return list(map(
+            lambda x: (x * (exp_eps + 1) / (exp_eps - 1) -
+                       meas_count / (exp_eps - 1)),
+            data
+        ))
+
+    def coin_flip(self):
+        return random.random() < self.p
 
 
 def test():
     rr = BinaryRandomizedResponse(8.0)
-    # Expect this to fail before it is implemented.
-    rr.sample_noise(100)
+    measurement = [0, 1, 0, 1]
+    noised_measurement = rr.add_noise(measurement)
+    assert(all(val in [0, 1] for val in noised_measurement))
+
+    sampled_noise = rr.sample_noise(100)
+    assert(all(val in [0, 1] for val in sampled_noise))
+
+    agg = [2, 1, 0, 1]
+    meas_count = 2
+    expected_debiased = [2.0006711504016828, 1.0, -0.0006711504016824899, 1.0]
+    actual_debiased = rr.debias(agg, meas_count)
+    for (actual, expected) in zip(actual_debiased, expected_debiased):
+        assert(math.isclose(actual, expected, abs_tol = 1e-6))
 
 
 if __name__ == '__main__':
--- poc/policy/policy.py
+++ poc/policy/policy.py
@@ -8,6 +8,18 @@ parties that participate in adding noise depends on the threat model that the
 DP policy is designed for.
 """
 
+# Access files that live in different directories, since currently the source
+# files are not module-based.
+import os
+import sys
+# `dir_name` points to the `poc` folder in DP draft.
+dir_name = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# Access files in VDAF `poc` folder.
+sys.path.append(os.path.join(dir_name, "draft-irtf-cfrg-vdaf", "poc"))
+from common import gen_rand
+from vdaf import Vdaf, pretty_print_vdaf_test_vec, to_le_bytes
+
+
 class DpPolicy:
     # Client measurement type.
     Measurement = None
@@ -46,3 +58,153 @@ class DpPolicy:
         `meas_count`. The default implementation is to do nothing.
         """
         return agg_result
+
+
+# NOTE This is used to generate {{run-dp-policy-with-vdaf}}.
+def run_dp_policy_with_vdaf(
+    dp_policy: DpPolicy,
+    Vdaf: Vdaf,
+    agg_param: Vdaf.AggParam,
+    measurements: list[DpPolicy.Measurement],
+    print_test_vec=False,
+    test_vec_instance=0,
+):
+    """Run the DP policy with VDAF on a list of measurements."""
+    nonces = [gen_rand(Vdaf.NONCE_SIZE) for _ in range(len(measurements))]
+    verify_key = gen_rand(Vdaf.VERIFY_KEY_SIZE)
+
+    # REMOVE ME
+    test_vec = {
+        'shares': Vdaf.SHARES,
+        'verify_key': verify_key.hex(),
+        'agg_param': agg_param,
+        'prep': [],
+        'agg_shares': [],
+        'agg_result': None,  # set below
+    }
+    type_params = Vdaf.test_vec_set_type_param(test_vec)
+
+    out_shares = []
+    for (nonce, measurement) in zip(nonces, measurements):
+        assert len(nonce) == Vdaf.NONCE_SIZE
+
+        # REMOVE ME
+        prep_test_vec = {
+            'measurement': measurement,
+            'nonce': nonce.hex(),
+            'public_share': None,  # set below
+            'input_shares': [],
+            'prep_shares': [[] for _ in range(Vdaf.ROUNDS)],
+            'prep_messages': [],
+            'out_shares': [],
+        }
+
+        # Each Client adds Client-DP noise to its measurement.
+        noisy_measurement = \
+            dp_policy.add_noise_to_measurement(measurement)
+        # Each Client shards its measurement into input shares.
+        rand = gen_rand(Vdaf.RAND_SIZE)
+        (public_share, input_shares) = \
+            Vdaf.shard(noisy_measurement, nonce, rand)
+
+        # REMOVE ME
+        prep_test_vec['rand'] = rand.hex()
+        prep_test_vec['public_share'] = \
+            Vdaf.test_vec_encode_public_share(public_share).hex()
+        for input_share in input_shares:
+            prep_test_vec['input_shares'].append(
+                Vdaf.test_vec_encode_input_share(input_share).hex())
+
+        # Each Aggregator initializes its preparation state.
+        prep_states = []
+        outbound = []
+        for j in range(Vdaf.SHARES):
+            (state, share) = Vdaf.prep_init(verify_key, j,
+                                            agg_param,
+                                            nonce,
+                                            public_share,
+                                            input_shares[j])
+            prep_states.append(state)
+            outbound.append(share)
+        # REMOVE ME
+        for prep_share in outbound:
+            prep_test_vec['prep_shares'][0].append(
+                Vdaf.test_vec_encode_prep_share(prep_share).hex())
+
+        # Aggregators recover their output shares.
+        for i in range(Vdaf.ROUNDS-1):
+            prep_msg = Vdaf.prep_shares_to_prep(agg_param,
+                                                outbound)
+            # REMOVE ME
+            prep_test_vec['prep_messages'].append(
+                Vdaf.test_vec_encode_prep_msg(prep_msg).hex())
+
+            outbound = []
+            for j in range(Vdaf.SHARES):
+                out = Vdaf.prep_next(prep_states[j], prep_msg)
+                (prep_states[j], out) = out
+                outbound.append(out)
+            # REMOVE ME
+            for prep_share in outbound:
+                prep_test_vec['prep_shares'][i+1].append(
+                    Vdaf.test_vec_encode_prep_share(prep_share).hex())
+
+        # The final outputs of the prepare phase are the output shares.
+        prep_msg = Vdaf.prep_shares_to_prep(agg_param,
+                                            outbound)
+        # REMOVE ME
+        prep_test_vec['prep_messages'].append(
+            Vdaf.test_vec_encode_prep_msg(prep_msg).hex())
+        outbound = []
+        for j in range(Vdaf.SHARES):
+            out_share = Vdaf.prep_next(prep_states[j], prep_msg)
+            outbound.append(out_share)
+
+        # REMOVE ME
+        for out_share in outbound:
+            prep_test_vec['out_shares'].append([
+                to_le_bytes(x.as_unsigned(), x.ENCODED_SIZE).hex()
+                for x in out_share
+            ])
+        test_vec['prep'].append(prep_test_vec)
+
+        out_shares.append(outbound)
+
+    num_measurements = len(measurements)
+    # Each Aggregator aggregates its output shares into an
+    # aggregate share, and adds any Aggregator-DP noise to its
+    # aggregate share. In a distributed VDAF computation, the
+    # aggregate shares are sent over the network.
+    agg_shares = []
+    for j in range(Vdaf.SHARES):
+        out_shares_j = [out[j] for out in out_shares]
+        agg_share_j = Vdaf.aggregate(agg_param, out_shares_j)
+        # Each Aggregator independently adds noise to its aggregate
+        # share.
+        noised_agg_share_j = \
+            dp_policy.add_noise_to_agg_share(agg_share_j)
+        agg_shares.append(noised_agg_share_j)
+
+        # REMOVE ME
+        test_vec['agg_shares'].append(
+            Vdaf.test_vec_encode_agg_share(noised_agg_share_j).hex())
+
+    # Collector unshards the aggregate.
+    agg_result = Vdaf.unshard(agg_param, agg_shares,
+                              num_measurements)
+    # Debias aggregate result.
+    debiased_agg_result = dp_policy.debias_agg_result(
+        agg_result, num_measurements
+    )
+    # REMOVE ME
+    test_vec['agg_result'] = debiased_agg_result
+    if print_test_vec:
+        pretty_print_vdaf_test_vec(Vdaf, test_vec, type_params)
+
+        os.system('mkdir -p test_vec/{:02}'.format(VERSION))
+        with open('test_vec/{:02}/{}_{}.json'.format(
+                VERSION, Vdaf.test_vec_name, test_vec_instance), 'w') as f:
+            json.dump(test_vec, f, indent=4, sort_keys=True)
+            f.write('\n')
+
+    return debiased_agg_result
